499a1e55e2eb4a54f1f7d828803b270d2465c589
Elias-Van-Beurden-Hydroteam <elias.vanbeurden1@hydroteam.be>
2025-05-08 14:48:29 +0200

    usb_test

--- demo/image_to_stm.py
+++ demo/image_to_stm.py
@@ -6,21 +6,21 @@ PORT = '/dev/ttyACM0'
 BAUD = 115200
 # Change to the directory you want to choose your image from. Or you can just change to /
 
-filename = filedialog.askopenfilename(initialdir = "/",
-                                          title = "Select a File",
-                                          filetypes = (("Images",
-                                                        "*.jpg*"),
-                                                       ("all files",
-                                                        "*.*")))
+# filename = filedialog.askopenfilename(initialdir = "/",
+#                                           title = "Select a File",
+#                                           filetypes = (("Images",
+#                                                         "*.jpg*"),
+#                                                        ("all files",
+#                                                         "*.*")))
 
-# print(filename)
+# # print(filename)
 
-with open(filename, 'rb') as image_file:
-    byte_data = image_file.read()
+# with open(filename, 'rb') as image_file:
+#     byte_data = image_file.read()
 
-# Transform byte_data(hexadecimal format?) into a list of u8 integers
-u8_array = list(byte_data)
-# print(u8_array)
+# # Transform byte_data(hexadecimal format?) into a list of u8 integers
+# u8_array = list(byte_data)
+# # print(u8_array)
 print(f"Loaded {len(u8_array)} bytes")
 
 with serial.Serial(PORT, BAUD, timeout=1) as ser:
@@ -32,12 +32,12 @@ with serial.Serial(PORT, BAUD, timeout=1) as ser:
 
 
 
-# writing to file
-new_text_file = open('/run/media/dikketrien/NOD_H755ZIQ/myfile.txt', 'w')
-new_text_file.write("[")
-for byte in u8_array[:-1]:
-    new_text_file.write(f"{byte}, ")
-new_text_file.write(f"{u8_array[-1]}")
+# # writing to file
+# new_text_file = open('/run/media/dikketrien/NOD_H755ZIQ/myfile.txt', 'w')
+# new_text_file.write("[")
+# for byte in u8_array[:-1]:
+#     new_text_file.write(f"{byte}, ")
+# new_text_file.write(f"{u8_array[-1]}")
 
-new_text_file.write("]")
-new_text_file.close()
+# new_text_file.write("]")
+# new_text_file.close()
---
+++ src/bin/usb_test.rs
@@ -0,0 +1,114 @@
+#![no_std]
+#![no_main]
+
+use core::mem::MaybeUninit;
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_futures::join::join;
+use embassy_stm32::usb::{Driver, Instance};
+use embassy_stm32::{bind_interrupts, peripherals, usb, Config, SharedData};
+use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use embassy_usb::driver::EndpointError;
+use embassy_usb::Builder;
+use {defmt_rtt as _, panic_probe as _};
+
+#[link_section = ".ram_d3.shared_data"]
+static SHARED_DATA: MaybeUninit<SharedData> = MaybeUninit::uninit();
+
+bind_interrupts!(struct Irqs {
+    OTG_FS => usb::InterruptHandler<peripherals::USB_OTG_FS>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Starting USB serial on STM32H755ZI...");
+
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hsi = Some(HSIPrescaler::DIV1);
+        config.rcc.csi = true;
+        config.rcc.hsi48 = Some(Hsi48Config { sync_from_usb: true }); // Required for USB FS
+        config.rcc.pll1 = Some(Pll {
+            source: PllSource::HSI,
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL50,
+            divp: Some(PllDiv::DIV2),
+            divq: Some(PllDiv::DIV8),
+            divr: None,
+        });
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.ahb_pre = AHBPrescaler::DIV2;
+        config.rcc.apb1_pre = APBPrescaler::DIV2;
+        config.rcc.apb2_pre = APBPrescaler::DIV2;
+        config.rcc.apb3_pre = APBPrescaler::DIV2;
+        config.rcc.apb4_pre = APBPrescaler::DIV2;
+        config.rcc.voltage_scale = VoltageScale::Scale1;
+        config.rcc.supply_config = SupplyConfig::DirectSMPS;
+        config.rcc.mux.usbsel = mux::Usbsel::HSI48;
+    }
+
+    let p = embassy_stm32::init_primary(config, &SHARED_DATA);
+
+    let mut ep_out_buffer = [0u8; 256];
+    let mut usb_config = embassy_stm32::usb::Config::default();
+    usb_config.vbus_detection = false;
+
+    let driver = Driver::new_fs(p.USB_OTG_FS, Irqs, p.PA12, p.PA11, &mut ep_out_buffer, usb_config);
+
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB Serial");
+    config.serial_number = Some("12345678");
+
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut [],
+        &mut control_buf,
+    );
+
+    let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
+    let mut usb = builder.build();
+
+    let usb_fut = usb.run();
+    let echo_fut = async {
+        loop {
+            class.wait_connection().await;
+            info!("USB connected!");
+            let _ = echo(&mut class).await;
+            info!("USB disconnected!");
+        }
+    };
+
+    join(usb_fut, echo_fut).await;
+}
+
+struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(e: EndpointError) -> Self {
+        match e {
+            EndpointError::BufferOverflow => defmt::panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+async fn echo<'d, T: Instance + 'd>(class: &mut CdcAcmClass<'d, Driver<'d, T>>) -> Result<(), Disconnected> {
+    let mut buf = [0; 64];
+    loop {
+        let n = class.read_packet(&mut buf).await?;
+        let data = &buf[..n];
+        info!("Received: {:x}", data);
+        class.write_packet(data).await?;
+    }
+}
