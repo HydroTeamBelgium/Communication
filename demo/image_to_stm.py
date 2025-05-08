856c5f77b021bf6500f06b750972f3f72a63a6d8
Louis-van-Sitteren-Martens-Hydro <louis.vansitterenmartens@hydroteam.be>
2025-05-08 14:28:29 +0200

    serial comm

--- demo/image_to_stm.py
+++ demo/image_to_stm.py
@@ -1,6 +1,9 @@
 from tkinter import filedialog
+import serial
+import time
 
-
+PORT = '/dev/ttyACM0'
+BAUD = 115200
 # Change to the directory you want to choose your image from. Or you can just change to /
 
 filename = filedialog.askopenfilename(initialdir = "/",
@@ -20,8 +23,17 @@ u8_array = list(byte_data)
 # print(u8_array)
 print(f"Loaded {len(u8_array)} bytes")
 
+with serial.Serial(PORT, BAUD, timeout=1) as ser:
+    print(f"connected to {PORT}")
+
+    message = "Hello blabla"
+    ser.write(message.encode('utf-8'))
+    print('clear')
+
+
+
 # writing to file
-new_text_file = open('demo/myfile.txt', 'w')
+new_text_file = open('/run/media/dikketrien/NOD_H755ZIQ/myfile.txt', 'w')
 new_text_file.write("[")
 for byte in u8_array[:-1]:
     new_text_file.write(f"{byte}, ")
