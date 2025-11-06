b7249df2fbf32aecabcb14e45fcb8c20e217e88a
Louis-van-Sitteren-Martens-Hydro <louis.vansitterenmartens@hydroteam.be>
2025-11-06 15:02:03 +0100

    files for receiver and sender

---
+++ src/bin/blinky.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+
+use core::mem::MaybeUninit;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::{Config, SharedData};
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+
+#[unsafe(link_section = ".ram_d3.shared_data")]
+static SHARED_DATA: MaybeUninit<SharedData> = MaybeUninit::uninit();
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let config = Config::default();
+    let p = embassy_stm32::init_primary(config, &SHARED_DATA);    
+    info!("Hello World!");
+
+    let mut led = Output::new(p.PE1, Level::High, Speed::Low);
+
+    loop {
+        info!("high");
+        led.set_high();
+        Timer::after_millis(2000).await;
+
+        info!("low");
+        led.set_low();
+        Timer::after_millis(1000).await;
+    }
+}
